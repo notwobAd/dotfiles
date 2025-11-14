d4febb43fbb934304fd7c10258952820804b560f
not2bAd <2720701658@qq.com>
2025-11-14 12:16:25 +0800

    chore(nvim): add javascriptreact to langs.lua

--- nvim/lua/core/langs.lua
+++ nvim/lua/core/langs.lua
@@ -1,7 +1,7 @@
 local bo = vim.bo
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "typescript", "javascript", "vue", "yaml" },
+	pattern = { "typescript", "javascript", "javascriptreact", "vue", "yaml" },
 	callback = function()
 		bo.tabstop = 2
 		bo.shiftwidth = 0
