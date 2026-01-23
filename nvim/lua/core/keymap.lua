1e7fb7a1bdc51e84a60fff83f2138d544cf73729
not2bAd <2720701658@qq.com>
2026-01-24 02:51:22 +0800

    chore(nvim): remove colons and change comments

--- nvim/lua/core/keymap.lua
+++ nvim/lua/core/keymap.lua
@@ -8,23 +8,23 @@ keymap.set("n", "+", "<C-a>")
 keymap.set("n", "-", "<C-x>")
 
 -- Undo/redo
-keymap.set({ "n", "i" }, "<C-z>", "<Cmd>:undo<Return>", opts)
-keymap.set({ "n", "i" }, "<C-y>", "<Cmd>:redo<Return>", opts)
+keymap.set({ "n", "i" }, "<C-z>", "<Cmd>undo<Return>", opts)
+keymap.set({ "n", "i" }, "<C-y>", "<Cmd>redo<Return>", opts)
 
 -- Save
-keymap.set({ "n", "i" }, "<C-s>", "<Cmd>:w!<Return>", opts)
+keymap.set({ "n", "i" }, "<C-s>", "<Cmd>w!<Return>", opts)
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G", opts)
 -- Quit
-keymap.set("n", "<C-q>", "<Cmd>:q<Return>", opts)
+keymap.set("n", "<C-q>", "<Cmd>q<Return>", opts)
 
 -- Vsplit/split
-keymap.set("n", "sv", "<Cmd>:vsplit<Return>", opts)
-keymap.set("n", "ss", "<Cmd>:split<Return>", opts)
-keymap.set("n", "<leader>sv", "<Cmd>:vsplit<Return>", opts)
-keymap.set("n", "<leader>ss", "<Cmd>:split<Return>", opts)
+keymap.set("n", "sv", "<Cmd>vsplit<Return>", opts)
+keymap.set("n", "ss", "<Cmd>split<Return>", opts)
+keymap.set("n", "<leader>sv", "<Cmd>vsplit<Return>", opts)
+keymap.set("n", "<leader>ss", "<Cmd>split<Return>", opts)
 
--- Move window
+-- Move cursor in neighbor windows
 keymap.set("n", "sk", "<C-w>k")
 keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sh", "<C-w>h")
@@ -41,11 +41,11 @@ keymap.set("n", "<C-w><left>", "<C-w><")
 keymap.set("n", "<C-w><right>", "<C-w>>")
 
 -- Use terminal
-keymap.set({ "n", "i" }, "<C-`>", "<Cmd>:terminal<Return>", opts)
+keymap.set({ "n", "i" }, "<C-`>", "<Cmd>terminal<Return>", opts)
 keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- Lazy
-keymap.set("n", "<leader>L", ":Lazy<Return>", opts)
+keymap.set("n", "<leader>L", "<Cmd>Lazy<Return>", opts)
 
 -- Go to next 10 lines
 keymap.set("n", "D", "10j")
