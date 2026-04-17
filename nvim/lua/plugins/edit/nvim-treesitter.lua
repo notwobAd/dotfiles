6244cf848e93abb8ac6284d2ea5f7fb8155bc3b2
not2bAd <2720701658@qq.com>
2026-04-17 16:33:17 +0800

    fix(nvim): auto enable treesitter

--- nvim/lua/plugins/edit/nvim-treesitter.lua
+++ nvim/lua/plugins/edit/nvim-treesitter.lua
@@ -8,8 +8,10 @@ local ensure_installed = {
 	"markdown_inline",
 	"cpp",
 	"typescript",
+	"typescriptreact",
 	"tsx",
 	"javascript",
+	"javascriptreact",
 	"vue",
 	"html",
 	"css",
@@ -25,6 +27,13 @@ return {
 	config = function(_, opts)
 		local nvim_treesitter = require("nvim-treesitter")
 		nvim_treesitter.setup(opts)
-		nvim_treesitter.install(ensure_installed)
+		nvim_treesitter.install(nvim_treesitter.get_available(ensure_installed))
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = ensure_installed,
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
 	end,
 }
