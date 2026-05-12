10f7a1e5f5db2a31b8da4fc6b7788e88439600d9
not2bAd <2720701658@qq.com>
2026-05-12 18:17:53 +0800

    fix(nvim): indent with 2 chars for jsonc

--- nvim/lua/core/langs.lua
+++ nvim/lua/core/langs.lua
@@ -1,7 +1,16 @@
 local bo = vim.bo
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue", "yaml", "json" },
+	pattern = {
+		"typescript",
+		"typescriptreact",
+		"javascript",
+		"javascriptreact",
+		"vue",
+		"yaml",
+		"json",
+		"jsonc",
+	},
 	callback = function()
 		bo.tabstop = 2
 		bo.shiftwidth = 0
