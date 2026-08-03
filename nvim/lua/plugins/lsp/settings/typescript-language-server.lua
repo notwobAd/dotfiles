c7555a2c3f51aa048082578fb4e429c7104e465e
not2bAd <2720701658@qq.com>
2026-08-04 01:25:41 +0800

    chore(nvim): replace vtsls with ts_ls

---
+++ nvim/lua/plugins/lsp/settings/typescript-language-server.lua
@@ -0,0 +1,17 @@
+local vue_typescript_plugin_path = vim.fn.stdpath("data")
+	.. "/mason/packages/vue-language-server/node_modules/@vue/typescript-plugin"
+
+local vue_plugin = {
+	name = "@vue/typescript-plugin",
+	location = vue_typescript_plugin_path,
+	languages = { "javascript", "typescript", "vue" },
+}
+
+return {
+	init_options = {
+		plugins = {
+			vue_plugin,
+		},
+	},
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+}
