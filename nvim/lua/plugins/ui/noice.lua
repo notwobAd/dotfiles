fc4e6300a780d81b279809f44efeab84af155370
not2bAd <2720701658@qq.com>
2026-08-19 20:27:26 +0800

    feat(nvim): skip notify 'No information available'

--- nvim/lua/plugins/ui/noice.lua
+++ nvim/lua/plugins/ui/noice.lua
@@ -24,5 +24,16 @@ return {
 				},
 			},
 		},
+		routes = {
+			{
+				filter = {
+					event = "notify",
+					find = "No information available",
+				},
+				opts = {
+					skip = true,
+				},
+			},
+		},
 	},
 }
